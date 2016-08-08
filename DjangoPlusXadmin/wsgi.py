bfbb502c4c03a681e9d34f440901959df36aa8e0
wfan <wfan@posbao.net>
2016-08-08 19:18:10 +0800

    new file:   DjangoPlusXadmin/__init__.py 	new file:   DjangoPlusXadmin/settings.py 	new file:   DjangoPlusXadmin/urls.py 	new file:   DjangoPlusXadmin/wsgi.py 	new file:   manage.py

---
+++ DjangoPlusXadmin/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for DjangoPlusXadmin project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "DjangoPlusXadmin.settings")
+
+application = get_wsgi_application()
