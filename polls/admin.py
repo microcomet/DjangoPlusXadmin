369284c38942c5c57090022f01712d4c7cf913c1
wfan <wfan@posbao.net>
2016-08-09 18:27:38 +0800

    modified:   DjangoPlusXadmin/settings.py 	modified:   polls/admin.py 	modified:   polls/models.py 	new file:   templates/admin/base_site.html

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,27 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Choice
+
+
+# class ChoiceInline(admin.StackedInline):
+#     model = Choice
+#     extra = 0
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 0
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'question_text', 'pub_date', 'was_published_recently')
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date Information', {'fields': ['pub_date']}),
+    ]
+    inlines = [ChoiceInline]
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+    # fields = ['pub_date', 'question_text']
 
 # Register your models here.
-admin.site.register(Question)
+admin.site.register(Question, QuestionAdmin)
